42360a4c40f7674a40f3de872fe8a68e2c2936d8
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-09 12:03:09 +0000

    corrigido e adicionado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,32 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
   stack = {
-    frontend: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'JavaScript', 'Angular', 'React', 'Vue.js'],
+    frontend: [
+      'HTML5',
+      'CSS3',
+      'Sass',
+      'Bootstrap',
+      'JavaScript',
+      'Angular',
+      'React',
+      'Vue.js',
+    ],
     backend: ['Node.js', 'Express'],
     databases: ['MongoDB', 'MySQL'],
     devops: ['GitHub', 'VSCode', 'Vercel', 'NPM'],
-    cms: ['WordPress', 'SPA moderna', 'API serverless (Node.js + Firebase Functions)'],
+    cms: [
+      'WordPress',
+      'SPA moderna',
+      'API serverless (Node.js + Firebase Functions)',
+    ],
   };
 }
