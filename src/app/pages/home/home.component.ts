5d944d28dadc2b08c3108861a15638432d5e0314
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-09 11:40:39 +0000

    refatorado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home-page',
-  imports: [],
+  selector: 'app-home',
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-
+  stack = {
+    frontend: ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'JavaScript', 'Angular', 'React', 'Vue.js'],
+    backend: ['Node.js', 'Express'],
+    databases: ['MongoDB', 'MySQL'],
+    devops: ['GitHub', 'VSCode', 'Vercel', 'NPM'],
+    cms: ['WordPress', 'SPA moderna', 'API serverless (Node.js + Firebase Functions)']
+  };
 }
