482b43a5aa79b815366199d65a48fb7a41e3893f
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-09 18:13:09 +0000

    alterado

--- src/app/components/stack-pill/stack-pill.component.ts
+++ src/app/components/stack-pill/stack-pill.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stack-pill',
   standalone: true,
+  imports: [CommonModule], 
   templateUrl: './stack-pill.component.html',
-  styleUrls: ['./stack-pill.component.css']
+  styleUrls: ['./stack-pill.component.scss'] 
 })
-
 export class StackPillComponent {
   @Input() name!: string;
   @Input() icon?: string;
