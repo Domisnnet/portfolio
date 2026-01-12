a79d7f80ec14ff2f2cd963bc8c658dd3a786813b
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-12 16:18:05 +0000

    arrumei a cagada

--- src/app/components/stack-pill/stack-pill.component.ts
+++ src/app/components/stack-pill/stack-pill.component.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stack-pill',
-  standalone: true, 
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './stack-pill.component.html',
   styleUrls: ['./stack-pill.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    '[class]': 'category()', 
+    '[class]': 'category()',
+    '[style.--delay]': 'delay()'
   }
 })
-
 export class StackPillComponent {
   name = input<string>('');
   iconPath = input<string>('');
+  delay = input<number>(0);
   category = input<'frontend' | 'backend' | 'database' | 'devops' | 'cms'>('frontend');
 }
