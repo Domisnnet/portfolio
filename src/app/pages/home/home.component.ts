cb5a2df61514fc1236cce1f828340e6ea5fe7f7a
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-15 20:11:18 +0000

    ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,12 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { StackService } from '../../services/stack.service';
-import { StackPillComponent } from '../../components/stack-pill/stack-pill.component';
 import { PillCategory } from '../../constants/project-tags.config';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, StackPillComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  readonly stackService = inject(StackService);
 
   readonly categories: readonly PillCategory[] = [
     'frontend',
@@ -22,4 +15,6 @@ export class HomeComponent {
     'devops',
     'cms',
   ];
+
+  constructor(public readonly stackService: StackService) {}
 }
