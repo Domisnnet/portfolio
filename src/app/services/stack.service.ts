f050d18b2f398f9cedfbcf9c659dd0339783eb8d
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-16 18:09:47 +0000

    ajustado

--- src/app/services/stack.service.ts
+++ src/app/services/stack.service.ts
@@ -1,36 +1,15 @@
 import { Injectable } from '@angular/core';
-import { STACK_CONFIG, PillCategory, TagKey, StackPillData, } from '../constants/project-tags.config';
+import { STACK_CONFIG, PillCategory, TagKey } from '../constants/project-tags.config';
+import { PERSONAL_STACK } from '../constants/personal-stack.config';
 
-export interface StackPillResolved extends StackPillData { key: TagKey; }
-
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class StackService {
-  private readonly stackByCategory: Record<
-    PillCategory,
-    StackPillResolved[]
-  >;
 
-  constructor() {
-    this.stackByCategory = {
-      frontend: [],
-      backend: [],
-      databases: [],
-      devops: [],
-      cms: [],
-    };
-
-    for (const key of Object.keys(STACK_CONFIG) as TagKey[]) {
-      const pill = STACK_CONFIG[key];
-      this.stackByCategory[pill.category].push({
-        key,
-        ...pill,
-      });
-    }
+  getPersonalStack(category: PillCategory) {
+    return PERSONAL_STACK[category].map(key => ({
+      key,
+      ...STACK_CONFIG[key],
+    }));
   }
 
-  getStack(category: PillCategory): StackPillResolved[] {
-    return this.stackByCategory[category];
-  }
 }
