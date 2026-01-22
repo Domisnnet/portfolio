053ea5057e05c4ef49d87cca3e68a3d89a4b0e03
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-22 13:37:17 +0000

    refatorado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,22 @@
 import { Routes } from '@angular/router';
-import { ProjectsLabPage } from './pages/projetos-lab/pages/projects-lab.page';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'projects',
+    loadComponent: () => import('./pages/projects/projects-page.component').then(m => m.ProjectsPageComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about-page.component').then(m => m.AboutPageComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact-page.component').then(m => m.ContactPageComponent)
+  },
   {
     path: 'lab',
     loadComponent: () => import('./pages/projetos-lab/pages/projects-lab.page').then(m => m.ProjectsLabPage)
