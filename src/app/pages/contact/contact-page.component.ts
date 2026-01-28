eea9bddc7054ca5e71f10aac5c4086ce02c7cf9c
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-28 19:20:11 +0000

    ajuste

--- src/app/pages/contact/contact-page.component.ts
+++ src/app/pages/contact/contact-page.component.ts
@@ -1,9 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-contact-page',
+  standalone: true,
+  imports: [],
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./contact-page.component.scss']
 })
-export class ContactPageComponent {}
+export class ContactPageComponent {
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  constructor(private firestore: AngularFirestore) {}
+
+  onSubmit() {
+    this.firestore.collection('contacts').add(this.contactData)
+      .then(() => {
+        alert('Mensagem salva com sucesso!');
+        this.contactData = { name: '', email: '', message: '' }; 
+      })
+      .catch(err => {
+        console.error('Erro ao salvar mensagem:', err);
+        alert('Ocorreu um erro, tente novamente.');
+      });
+  }
+}
