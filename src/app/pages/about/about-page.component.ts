2b28796cbb8c57a5ca09fedf3340000609a7f967
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-28 14:28:31 +0000

    ajustados

--- src/app/pages/about/about-page.component.ts
+++ src/app/pages/about/about-page.component.ts
@@ -6,43 +6,43 @@ import aboutContent from '../../content/about-content.json';
 type Language = 'pt' | 'en';
 
 @Component({
-  selector: 'app-contact-page',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [
-    trigger('panelEnter', [
-      transition(':enter', [
-        style({
-          transform: 'translate(-50%, 100%) scale(0.85)',
-          opacity: 0
-        }),
-        animate(
-          '900ms cubic-bezier(0.34, 1.56, 0.64, 1)',
-          style({
-            transform: 'translate(-50%, -50%) scale(1)',
-            opacity: 1
-          })
-        )
-      ])
-    ])
-  ]
+    selector: 'app-about-page',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './about-page.component.html',
+    styleUrls: ['./about-page.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [
+        trigger('panelEnter', [
+            transition(':enter', [
+                style({
+                    transform: 'translate(-50%, 100%) scale(0.85)',
+                    opacity: 0
+                }),
+                animate(
+                    '900ms cubic-bezier(0.34, 1.56, 0.64, 1)',
+                    style({
+                        transform: 'translate(-50%, -50%) scale(1)',
+                        opacity: 1
+                    })
+                )
+            ])
+        ])
+    ]
 })
-export class ContactPageComponent {
+export class AboutPageComponent {
 
-  isTextMode = false;
-  language: Language = 'pt';
+    isTextMode = false;
+    language: Language = 'pt';
 
-  content = aboutContent[this.language];
+    content = aboutContent[this.language];
 
-  toggleMode(): void {
-    this.isTextMode = !this.isTextMode;
-  }
+    toggleMode(): void {
+        this.isTextMode = !this.isTextMode;
+    }
 
-  toggleLanguage(lang: Language): void {
-    this.language = lang;
-    this.content = aboutContent[this.language];
-  }
+    toggleLanguage(lang: Language): void {
+        this.language = lang;
+        this.content = aboutContent[this.language];
+    }
 }
