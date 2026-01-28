6e59f4a356542e729107905c914c80e818437e0b
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-28 18:14:09 +0000

    adicionados e ajustado

---
+++ src/app/components/footer/pages/2024/2024.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-2024',
+  templateUrl: './2024.component.html',
+  styleUrls: ['./2024.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Year2024Component {
+
+}
---
+++ src/app/components/footer/pages/domisdev-v1.0/domisdev-v1.0.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-domisdev-v1-0',
+  templateUrl: './domisdev-v1.0.component.html',
+  styleUrls: ['./domisdev-v1.0.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DomisdevV1Component {
+
+}
