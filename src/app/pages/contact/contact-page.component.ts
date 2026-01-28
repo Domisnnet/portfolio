6a754eec6bedc2d1d7f6bcb0cbd85db2554689c3
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-28 19:42:38 +0000

    OK

--- src/app/pages/contact/contact-page.component.ts
+++ src/app/pages/contact/contact-page.component.ts
@@ -1,27 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss']
+  styleUrls: ['./contact-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactPageComponent {
+  private firestore = inject(AngularFirestore);
+
   contactData = {
     name: '',
     email: '',
     message: ''
   };
 
-  constructor(private firestore: AngularFirestore) {}
-
   onSubmit() {
     this.firestore.collection('contacts').add(this.contactData)
       .then(() => {
         alert('Mensagem salva com sucesso!');
-        this.contactData = { name: '', email: '', message: '' }; 
+        this.contactData = { name: '', email: '', message: '' };
       })
       .catch(err => {
         console.error('Erro ao salvar mensagem:', err);
